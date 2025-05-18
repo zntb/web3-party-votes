24fc9a73ae036c2979484b5062c1092b65abbd36
zntb <zenetibi@yahoo.com>
2025-05-18 14:24:22 +0300

    Update hardhat.config - add @typechain/hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomicfoundation/hardhat-verify';
+import '@typechain/hardhat';
 
 const config: HardhatUserConfig = {
   solidity: {
