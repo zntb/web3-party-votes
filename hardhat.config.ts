24678f6b217def24f294d4fd57878016d2acb163
zntb <zenetibi@yahoo.com>
2025-05-18 14:18:54 +0300

    Update imports in hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-verify';
 
 const config: HardhatUserConfig = {
   solidity: {
